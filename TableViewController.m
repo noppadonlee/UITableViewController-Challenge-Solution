007742f2b76bdea0b5a157e268f745027d2df99f
Noppadon Lee <noppadon.lee@gmail.com>
2014-10-01 22:43:45 +0700

    solved the challenge

--- TableViewController.m
+++ TableViewController.m
@@ -56,13 +56,13 @@
     
     // Configure the cell...
     if (indexPath.section == 0) {
-        cell.textLabel.text = [NSString stringWithFormat:@"I'm in section 0"];
+        cell.textLabel.text = @"I'm in section 0";
         cell.backgroundColor = [UIColor redColor];
     } else if (indexPath.section == 1) {
-        cell.textLabel.text = [NSString stringWithFormat:@"another section"];
+        cell.textLabel.text = @"another section";
         cell.backgroundColor = [UIColor blueColor];
     } else {
-        cell.textLabel.text = [NSString stringWithFormat:@"%i", indexPath.row];
+        cell.textLabel.text = [NSString stringWithFormat:@"Cell %i", indexPath.row];
         cell.backgroundColor = [UIColor yellowColor];
     }
     
